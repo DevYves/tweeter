36bf596503a8517a9cff4cda277e1a0b721de3c7
DevYves <yfreypons@gmail.com>
2017-11-14 21:32:52 +0000

    implemented test to log user typing in textbox

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,13 @@
+console.log("Script loaded.");
+
 $(document).ready(function() {
+
   console.log("Document Ready.");
+
+  $(".new-tweet").on("input", function() {
+    console.log("MEOW.");
+  console.log($(".counter"));
+
+  });
 
 });
