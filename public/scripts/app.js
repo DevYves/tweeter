31ceacc840db0361cbc7a226e35b2bc29314bb4c
DevYves <yfreypons@gmail.com>
2017-11-16 02:07:46 +0000

    tweets now post immediately and alerts prevent empty or verbost posting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,82 +6,7 @@
 
 $(document).ready(function() {
 
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit. "
-    },
-    "created_at": 1461113796368
-  }
-];
-
-// var tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": {
-//       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//     },
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// }
-
-function renderTweets(tweets) {
-  for(var tweet in tweets) {
-    console.log("tweet in loop :", tweets);
-    console.log("hopefuly the object :", tweets[tweet]);
-    var createdTweet = createTweetElement(tweets[tweet]);
-    $('.tweetsWrapper').append(createdTweet);
-  }
-  return createdTweet;
-}
-
-// var tweet = createTweetElement(tweetData);
-
-function createTweetElement(tweetData){
+  function createTweetElement(tweetData){
   var header = $('<header>')
          .append(`<img class="avatar" src="${tweetData.user.avatars.regular}">`)
          .append(`<h3 class="username">${tweetData.user.name}</h2>`)
@@ -101,20 +26,68 @@ function createTweetElement(tweetData){
          .append(paragr)
          .append(footer);
 
-  // $('.tweetsWrapper').append(full);
-
   return full;
-
-
 
 }
 
-// var tweets = renderTweets(data);
+function renderTweets(tweets) {
+  for(var tweet in tweets) {
+    console.log("tweet in loop :", tweets);
+    console.log("hopefuly the object :", tweets[tweet]);
+    var createdTweet = createTweetElement(tweets[tweet]);
+    $('.tweetsWrapper').prepend(createdTweet);
+  }
+  return createdTweet;
+}
+
+function loadTweets() {
+  $.getJSON('/tweets')
+  .done((tweet) => {
+    renderTweets(tweet);
+
+  });
+
+}
+
+loadTweets();
+
+function submitTweets(){
+  $('#button').on('click', function(event){
+    event.preventDefault();
+  const $form = $(".textarea");
+  if ($form.serialize().length <= 5){
+    alert("Forms cannot be left empty!");
+    return;
+    } else if ($form.serialize().length > 145){
+      alert("Please keep messages to 140 characters");
+      return;
+    }
+
+  $.ajax({
+    method: 'post',
+    url:  '/tweets',
+    data: $form.serialize()
+
+  }).then(function(tweets){
+      loadTweets(tweets);
+      $form.val('');
+      $('.counter').text(140);
+    });
+})
+
+}
+
+submitTweets();
 
 
-// createTweetElement(tweets);
 
-renderTweets(data);
+
+
+
+// var tweet = createTweetElement(tweetData);
+
+
+
 
 });
 
--- public/scripts/new-tweets.js
+++ public/scripts/new-tweets.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-var tweetArea = $('article');
-var icons = $('div');
-tweetArea.mouseenter(function(){
-  tweetArea.css('opacity', 1);
-  icons.css('opacity', 1);
-});
-tweetArea.mouseleave(function(){
-  tweetArea.css('opacity', 0.9);
-  icons.css('opacity', 0);
-});
-});
+// $(document).ready(function(){
+// var tweetArea = $('article');
+// var icons = $('div');
+// tweetArea.mouseenter(function(){
+//   tweetArea.css('opacity', 1);
+//   icons.css('opacity', 1);
+// });
+// tweetArea.mouseleave(function(){
+//   tweetArea.css('opacity', 0.9);
+//   icons.css('opacity', 0);
+// });
+// });
 
